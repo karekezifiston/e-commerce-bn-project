af769ecb1799ae48740733dde43dd7145c7d7af6
karekezi fiston <karekezifiston30@gmail.com>
2025-09-08 16:26:50 +0200

    aaa

--- frontend/src/pages/FoodDetails.tsx
+++ frontend/src/pages/FoodDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
 import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react';
